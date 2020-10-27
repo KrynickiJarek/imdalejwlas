2252661966326bb7486ba31bbd22d35f17eceb80
KrynickiJarek <krynickijarek@gmail.com>
2020-10-27 10:52:33 +0100

    setting up CRA

---
+++ src/layouts/App/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Im dalej w las tym więcej JS</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
